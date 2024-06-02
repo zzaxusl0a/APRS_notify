a401ee1ec20d0d83b4abeed6974728d7761469de
zzaxusl0a <zzaxusl0a@hotmail.com>
2024-06-02 12:31:24 -0700

    Update APRS_SMS_processor.py

--- Lambda Code/APRS_SMS_processor.py
+++ Lambda Code/APRS_SMS_processor.py
@@ -6,12 +6,18 @@ import base64
 import hmac
 import boto3
 from hashlib import sha1, sha256
+from twilio.rest import Client
 
 
 #setup logger
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
+#setup environment variables
+INBOUND_WEBHOOK_URL = os.environ['REQUEST_URL']
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+MESSAGING_SERVICE_SID = os.environ['TWILIO_MSG_SERVICE_SID']
 
 
 def lambda_handler(event, context):
@@ -34,12 +40,17 @@ def lambda_handler(event, context):
     print(request_body.decode('utf8'))
     payload = request_body.decode('utf8')
     logger.info("inbound SMS body: " +payload)
-  catch:
-    lambda_return =  {'Status': '400', 'Message': 'Invalid SMS received', 'Code':'iSMS'}
-    return {
-     'statusCode': lambda_return['Status'],
-     'body': json.dumps(lambda_return)
-   }
+  except Exception as err:
+    #TODO: review exception - if no header, fail gracefully.
+    #if err.<graceful>:
+      lambda_return =  {'Status': '400', 'Message': 'Invalid SMS received', 'Code':'iSMS'}
+      return {
+        'statusCode': lambda_return['Status'],
+        'body': json.dumps(lambda_return)
+      }
+    #else
+      # logger.exception("Exception in SMS: " + (f"{type(err).__name__} was raised: {err}"))
+    #endif
   #endtry
 
   #at this point, we should have a valid twilio webhook and have decoded the body
@@ -68,20 +79,31 @@ def lambda_handler(event, context):
     #todo: do I need to put this in a try? how does this fail?
     callsign = text_body[1].upper()
 
-    #command response logic begins here     
+    #command response logic begins here  
+    #define the outbound status message variable
+    outbound_status_message = ""
+
     if(action_req == "START"):
       #if body is START: Identify callsign, Store number in DB, along with callsign, timestamp, and active flag. Return SMS opt-in message
       logger.info("Start message received from: " +callsign)
+      outbound_status_message = configure_cron_job(callsign, inbound_number, TRUE)
+      send_sms(inbound_number, outbound_status_message)
     elif(action_req == "STOP"):
       #if body is STOP: Identify callsign, remove callsign from DB. Return SMS message monitoring stopped
       logger.info("Stop message received from: " +callsign)
+      outbound_status_message = configure_cron_job(callsign, inbound_number, FALSE)
+      send_sms(inbound_number, outbound_status_message)
     elif(action_req == "STATUS"):
       #if body is STATUS: Identify callsign, return status from DB
       logger.info("Status message received from: " +callsign)
+      outbound_status_message = monitor_status(callsign)
+      logger.info("Monitor status for " +callsign +"is: " +outbound_status_message)
+      send_sms(inbound_number, outbound_status_message)
     else:
       #else: return help message or nothing
-      logger.info("invalid message received")
-   #endif
+      logger.info("invalid command received")
+      send_sms(inbound_number,"Invalid Command: Use START, STOP, or STATUS followed by callsign. ex. START AB1CDE")
+    #endif
                
    # if request is not from twilio, give appropriate response
    else:
@@ -97,7 +119,7 @@ def lambda_handler(event, context):
 
 def twilio_validator(signature, params)
   # if twilioSignature exists and message from authorized number, create a validator & a dictionary of received data
-  #s = os.environ['REQUEST_URL']
+  
   s = "https://wlbzgytolyvqirav53yrx4bm3q0ooaxi.lambda-url.us-west-2.on.aws/"
   if res:
     for param_name in sorted(set(res)):
@@ -114,10 +136,133 @@ def twilio_validator(signature, params)
   s += ":443"
 #end twilio_validator
 
-def send_sms(phone_number, body)
+#function sends sms message. Does not return a value, will throw exception if fails
+def send_sms(sms_to_number, message_body)
   #code to send a response SMS
-
+  try:
+     client = Client(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN)
+     message = client.messages.create(
+       messaging_service_sid= MESSAGE_SERVICE_SID,
+       to= sms_to_number,
+       body= message_body
+     )
+  except Exception as err:
+     logger.exception("Exception in SMS: " + (f"{type(err).__name__} was raised: {err}"))
+  #endtry
 #end send_sms
+
+#function to check the most recent APRS packet received and return human-readable string
+def monitor_status(callsign)
+  #set the return value
+  return_value = "not found"
+  
+  try:
+    #create a database connection to find most recent status
+    simpleDBclient = boto3.client('sdb')
+    getDB_response = simpleDBclient.get_attributes(
+      DomainName='APRS_tracker',
+      ItemName= callsign
+    )
+    #look for record for callsign
+    if "Attributes" in getDB_response:
+      #we have a record for this name, get the alert flag
+      for attribute in getDB_response["Attributes"]:
+        if attribute["Name"] == "alert_sent":
+          alert_sent = attribute["Value"]
+        #endif
+      #endfor
+      #extract details from the comment
+      for attribute in getDB_response["Attributes"]:
+        if attribute["Name"] == "comment":
+          previous_comment = attribute["Value"]
+          previous_recorded_temp = float(previous_comment[2:7])
+          #TODO: extract last recorded time - here or elsewhere?
+          #previous_reported_time = time.strftime('%Y-%m-%dT%H:%M:%SZ%z', time.localtime(lasttime_int))
+          previous_reported_time = "now"
+        #endif
+      #endfor
+      #TODO: find if cron job is still active, and for how long?
+      #we now have required variables, set the return value
+      return_value = "Most Recent report was: " +previous_recorded_temp +"F at: " +previous_reported_time)
+    else:
+       #we don't have a record
+       #TODO: should we look for the cron job?
+       return_value = "No record found for this callsign"
+     #endif
+  except Exception as err:
+        lambda_return['Status'] = "500"
+        lambda_return['Message'] = "Exception in Monitor: " +(f"{type(err).__name__} was raised: {err}")
+        lambda_return['Code'] = "MON"
+        
+        logger.exception(lambda_return['Message'])
+        return_value = "database error"
+  finally:
+      simpleDBclient.close()
+      return return_value 
+  #endtry
+#end monitor_status
+
+#manages the monitoring cron job. Requires a callsign, sms number, and boolean active flag
+#returns a human-readable status string
+#TODO: add some kind of abuse rate-limiting here
+def configure_cron_job(callsign, inbound_sms_number, montior_active)
+  try:
+    #set up the EventBridge scheduler object
+    EB_client = boto3.client('scheduler')
+    #parse the request
+    if(monitor_active):
+      #request is to create a new monitor, or edit an existing one
+      #may need an anti-abuse here to avoid extending past xx hours
+      #see: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler/client/create_schedule.html
+      #TODO: create a scheduling group called APRS_monitor
+      
+      return_value = "Monitoring active for: " +callsign
+    else:
+      #request is to delete monitor
+      #need to check that the request to delete is coming from the originating number
+      #see: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler/client/delete_schedule.html
+      #TODO: check all this - it's psuedocode!
+      #list schedule (returns dict)
+      requested_schedule_list = client.list_schedules(
+        GroupName='APRS_monitor',
+        NamePrefix=callsign
+      )
+      
+      #get schedule from list
+      requested_schedule = client.get_schedule(
+        GroupName='APRS_monitor',
+        Name=requested_schedule_list.schedules[0].Name
+      )
+  
+      #should have the correct schedule in the requested_schedule object. Check for phone number match
+      #in the input JSON
+      print(json.dumps(requested_schedule.Target.Input)
+        
+      if requested_schedule.Target.Input.Phone = inbound_sms_number:
+        #parse the request and delete monitor
+        response = client.delete_schedule(
+          GroupName='APRS_monitor',
+          Name=requested_schedule.Name
+        )
+        return_value = "Monitoring stopped for: " +callsign
+      else:
+         return_value = "You do not have permission to stop monitoring for: " +callsign
+      #endif
+    #endif
+
+  except Exception as err:
+     lambda_return['Status'] = "500"
+     lambda_return['Message'] = "Exception in SCH: " +(f"{type(err).__name__} was raised: {err}")
+     lambda_return['Code'] = "SCH"
+        
+     logger.exception(lambda_return['Message'])
+     return_value = "Exception occured. Monitoring not changed"
+  finally:
+    EB_client.close()
+    return return_value
+  #endtry
+#end configure_cron_job
+
 
 
 #eof
